f2c50238cbd0fc2b0a594239cdff363feedff971
gundorovnikita <gundorovnikita@yahoo.com>
2018-11-21 18:29:42 +0400

    Init

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Post, Categories
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     category = Categories.objects.all()
+    paginator = Paginator(posts, 4)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
     context= {
-        'posts':posts,
+        'contacts':contacts,
         'category':category,
     }
     return render(request, 'blog/index.html', context)
@@ -21,8 +25,11 @@ def post_detail(request, slug):
 def Categories_detail(request, slug):
     category = Categories.objects.get(slug=slug)
     pages = Post.objects.filter(category=category).order_by('-date')
+    paginator = Paginator(pages, 3) # Show 25 contacts per page
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
     context= {
         'category':category,
-        'pages':pages,
+        'contacts':contacts,
     }
     return render(request, 'blog/category.html' , context)
