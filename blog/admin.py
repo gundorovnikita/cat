2867ce5543ea4beda5fe8ebe6fb25fc4008f5d23
gundorovnikita <gundorovnikita@yahoo.com>
2018-11-21 18:10:08 +0400

    Init

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import *
 from django_summernote.admin import SummernoteModelAdmin
 
-class SomeModelAdmin(SummernoteModelAdmin):
+
+class SomeModelAdmin(SummernoteModelAdmin, admin.ModelAdmin):
         summernote_fields = '__all__'
+        list_display = ('title','category', 'slug', 'date')
+
 
 # Register your models here.
 admin.site.register(Post, SomeModelAdmin)
